3e48e6889f409eaac489d6f548cc207172b73693
Bot Anica <test.develop.user@gmail.com>
2025-08-07 12:55:20 +0300

    update base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/rss-bot-cource-landing/',
+  base: '/rss-bot-cource-landing',
   plugins: [
     react(),
     tailwindcss(),
